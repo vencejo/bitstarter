a202219f75b4ebd00ba673916f8148c779d611cb
vencejo <apussapus@gmail.com>
2013-07-08 18:25:16 +0200

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
+var fs = require('fs');
 var express = require('express');
+
+var dataFromIndex = null;
+
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+  dataFromIndex = data;
+});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(dataFromIndex);
 });
 
 var port = process.env.PORT || 5000;
